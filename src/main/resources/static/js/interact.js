bf7d1267703ffc4228dc85c0e95ea9f9943decd8
Les Mahon <les@gingertec.com>
2016-10-15 17:43:02 +0200

    ui- update

--- src/main/resources/static/js/interact.js
+++ src/main/resources/static/js/interact.js
@@ -3,22 +3,24 @@ var actJsonformatPublic = JSON.parse('{"act":[{"id":1088102,"code":"1","title":"
 var actDetailJsonformatPrivate = JSON.parse('{"act":[{"id":1088102,"code":"1","title":"This is the act 1","keywords":["Agriculture","Spain"],"visibility":"public","type":"directive","event":[{"id":1,"originating_institution":"Commission","destination_institution":["Parliament","Council"],"name":"startProcess","date":"01/01/2016","visibility":"public"},{"id":2,"originating_institution":"Commission","destination_institution":["Parliament","Council"],"name":"expertGroup","date":"01/03/2016","visibility":"private"}]}]}');
 var actDetailJsonformatPublic = JSON.parse('{"act":[{"id":1088102,"code":"1","title":"This is the act 1","keywords":["Agriculture","Spain"],"visibility":"public","type":"directive","event":[{"id":1,"originating_institution":"Commission","destination_institution":["Parliament","Council"],"name":"startProcess","date":"01/01/2016","visibility":"public"}]}]}');
 
-
 var USERROLE = 'NONE';
 
+var liveData = true;
+
 function addRow(act){
+    console.log(act);
     var grid = $("#example");
     var newRow = "<tr class='rowAction' data-value='"+act["id"]+"'><td>"+act["code"]+"</td><td>"+act["title"]+"</td><td>"+act["type"]+"</td><td>"+act["keywords"]+"</td></tr>";
     $('#example > tbody').append(newRow);
 };
 
 function drawTimeline(actId){
-    
+
     var actTimeline = actDetailJsonformatPublic["act"][0];
     if (USERROLE==='INSTITUTION') {
         actTimeline = actDetailJsonformatPrivate["act"][0];
     }
-    
+
     var events = actTimeline.event;
     var actName =actTimeline.title;
     var htmlString = '';
@@ -38,10 +40,10 @@ function drawTimeline(actId){
 }
 
 $(document).ready(function () {
-    
+
     $.ajax({
             type: "GET",
-            url: "/user",
+            url: "http://104.40.156.209:8080/user",
             async : false,
             success: function(data) {
                 if (data === 'INSTITUTION') {
@@ -61,7 +63,7 @@ $(document).ready(function () {
                     $('#loginLinkDetails').append('<a id="linkDetailLogout"><span class="glyphicon glyphicon-log-out"></span> Logout</a>');
                     $('#linkDetailLogout').bind("click", function(event) {
                         $.ajax({
-                            url: "/logout",
+                            url: "http://104.40.156.209:8080/logout",
                             success: function() {
                                 window.location.href = "/";
                             }
@@ -73,40 +75,86 @@ $(document).ready(function () {
                 }
             }
         });
-    
+
     //show grid and populate
     $("#searchButton").bind("click", function(){
         $("#grid").show();
         $(document).scrollTop($("#grid").offset().top);
     });
 
-    var acts = actJsonformatPublic["act"];
+    var url = "http://104.40.156.209:8080/v2/acts/private/list";
     if (USERROLE==='INSTITUTION') {
-        acts = actJsonformatPrivate["act"];
+        url = "http://104.40.156.209:8080/v2/acts/private/list";
     }
 
-    for (var index in acts) {      
-        addRow(acts[index]);
-    }
-    //show timeline and populate
-    $(".rowAction").bind("click", function(){
-      var actId = $(this).attr("data-value");
-      $("#timeline").show();
-      $(document).scrollTop($("#timeline").offset().top);
-      drawTimeline(actId);
-    });
-    
-    $('#btn-login').bind("click", function() {
-        $.ajax({
-            type: "POST",
-            url: "/login",
-            data: {
-                username: $('#login-username').val(),
-                password: $('#login-password').val()
-            },
-            success: function() {
-                window.location.href = "/";
-            }
-        });
-    })
+    if(liveData){
+      //get data from server http://10.40.11.237:8080/v2/acts/private/list    http://10.40.11.240:8080/v2/acts/private/list
+      $.getJSON( url, function( data ) {
+
+
+        $("#table").append('<div class="table-responsive" ><table id="example" class="table table-striped table-responsive display responsive nowrap" width="100%" cellspacing="0"><thead><tr><th data-priority="1">Code</th><th data-priority="2">Title</th><th data-priority="3">Type</th><th data-priority="4">Keywords</th></tr></thead><tbody></tbody></table></div>');
+
+          for (var index in data) {
+              addRow(data[index]);
+          }
+
+          var table = $('#example').DataTable({
+              //dom: 'lBfrtip',
+              //dom: 'Blf<t><"d-info"i>p',
+              responsive: true,
+              buttons: [
+                  'copyHtml5',
+                  'excelHtml5',
+                  'csvHtml5',
+                  'pdfHtml5'
+              ]
+          });
+          $('.d-panel-print').append(table.buttons().container());
+          $('.d-panel-print').append('<div style="clear: both"></div>');
+
+          //show timeline and populate
+          $(".rowAction").bind("click", function(){
+            var actId = $(this).attr("data-value");
+            $("#timeline").show();
+            $(document).scrollTop($("#timeline").offset().top);
+            drawTimeline(actId);
+          });
+
+       });
+
+       $('#btn-login').bind("click", function() {
+           $.ajax({
+               type: "POST",
+               url: "/login",
+               data: {
+                   username: $('#login-username').val(),
+                   password: $('#login-password').val()
+               },
+               success: function() {
+                   window.location.href = "/";
+               }
+           });
+       })
+
+     } else {
+      var acts = actJsonformat["act"];
+
+       for (var index in acts) {
+           addRow(acts[index]);
+       }
+
+       $('#btn-login').bind("click", function() {
+           $.ajax({
+               type: "POST",
+               url: "/login",
+               data: {
+                   username: $('#login-username').val(),
+                   password: $('#login-password').val()
+               },
+               success: function() {
+                   window.location.href = "/";
+               }
+           });
+       })
+     }
 });
