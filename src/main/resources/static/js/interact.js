3fcd474f5e0096c8ac37afffaf008375abc20be6
carlos-torrecilla <carlosjtorrecilla@MacBook-Pro-de-Carlos-Torrecilla-2.local>
2016-10-15 14:04:58 +0200

    js to populate the grid

--- src/main/resources/static/js/interact.js
+++ src/main/resources/static/js/interact.js
@@ -1,5 +1,7 @@
-function addRow(element){
-    console.log(element);
+function addRow(act){  
+    var grid = $("#example");
+    var newRow = "<tr><td>"+act["code"]+"</td><td>"+act["title"]+"</td><td>"+act["type"]+"</td><td>"+act["keywords"]+"</td></tr>";
+    $('#example > tbody').append(newRow);
 };
 
 $(document).ready(function () {
@@ -8,11 +10,12 @@ $(document).ready(function () {
         $("#grid").show();
     });
 
-    var actJsonformat = JSON.parse('{"act": [{"id": "1088102","code": "1","title": "This is the act 1","keywords": ["Agriculture", "Spain"],"visibility": "public","type": "directive","event": [{"id": 1,"originating_institution": "Commission","destination_institution": ["Parliament", "Council"],"name": "startProcess","date": "01/01/2016","visibility": "public"}]}]}');
+    var actJsonformat = JSON.parse('{"act":[{"id":1088102,"code":"1","title":"This is the act 1","keywords":["Agriculture","Spain"],"visibility":"public","type":"directive","event":[{"id":1,"originating_institution":"Commission","destination_institution":["Parliament","Council"],"name":"startProcess","date":"01/01/2016","visibility":"public"}]},{"id":1088103,"code":"2","title":"This is the act 2","keywords":["Fisheries","Ireland"],"visibility":"public","type":"decision","event":[{"id":3,"originating_institution":"Commission","destination_institution":["Parliament","Council"],"name":"adoption","date":"01/02/2016","visibility":"public"}]},{"id":1088104,"code":"3","title":"This is the act 3","keywords":["IT","internal"],"visibility":"private","type":"delegatedAct","event":[{"id":4,"originating_institution":"Parliament","destination_institution":["Parliament"],"name":"plenary","date":"01/03/2016","visibility":"private"}]},{"id":1088105,"code":"4","title":"This is the act 3","keywords":["Home affairs","Justice","Treaty"],"visibility":"public","type":"directive","event":[{"id":7,"originating_institution":"Council","destination_institution":["Commission","Council"],"name":"approve","date":"01/04/2016","visibility":"public"}]}]}');
     
-    actJsonformat.foreach(addRow());
+    var acts = actJsonformat["act"];
 
-
-    var grid = $("#example");
-
+    for (var index in acts) {
+        addRow(acts[index]);
+    }
+    
 });
